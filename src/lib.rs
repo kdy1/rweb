8f9e1b27bd578a33a5c090f4c6b85483d2f3ede9
강동윤 <kdy1997.dev@gmail.com>
2020-01-18 18:12:55 +0900

    Add extract Module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-pub use self::{app::App, response::Response};
+pub use self::{app::App, extract::FromRequest, response::Response};
 pub use rweb_macros::{connect, delete, get, head, options, patch, post, put, trace};
 
 mod app;
 pub mod cookie;
 pub mod encoding;
 pub mod error;
+mod extract;
 pub mod guard;
 pub mod http;
 pub mod resource;
