90835ff7d54e96b91f432d93424993c0d5ec1712
강동윤 <kdy1997.dev@gmail.com>
2020-01-18 17:53:08 +0900

    Add crate::error::UrlGenerationError

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,24 @@
-use derive_more::From;
+use derive_more::{Display, From};
+use url::ParseError as UrlParseError;
 
 pub trait ResponseError {}
+
+/// Errors which can occur when attempting to generate resource uri.
+#[derive(Debug, PartialEq, Display, From)]
+pub enum UrlGenerationError {
+    /// Resource not found
+    #[display(fmt = "Resource not found")]
+    ResourceNotFound,
+    /// Not all path pattern covered
+    #[display(fmt = "Not all path pattern covered")]
+    NotEnoughElements,
+    /// URL parse error
+    #[display(fmt = "{}", _0)]
+    ParseError(UrlParseError),
+}
+
+/// `InternalServerError` for `UrlGeneratorError`
+impl ResponseError for UrlGenerationError {}
 
 #[derive(From)]
 pub struct Error {
