fcd9b50708bc7180567286a84f2c312a6955b335
강동윤 <kdy1997.dev@gmail.com>
2020-01-19 10:25:26 +0900

    Add hyper module

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -11,6 +11,8 @@ use http::Extensions;
 use rweb_service::{apply, boxed, IntoServiceFactory, ServiceFactory, Transform};
 use std::{fmt, future::Future, rc::Rc};
 
+mod hyper;
+
 /// An application builder.
 pub struct App<S, Body>
 where
