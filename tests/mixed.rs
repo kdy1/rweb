3d069f53eccd9637a4ac76979a3c4bed2652eade
강동윤 <kdy1997.dev@gmail.com>
2020-01-19 19:47:06 +0900

    test: body after path parameter

---
+++ tests/mixed.rs
@@ -0,0 +1,34 @@
+use http::Error;
+use rweb::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct LoginForm {
+    id: String,
+    password: String,
+}
+
+#[get("/param/{foo}")]
+fn body_after_path_param(foo: String, #[json] body: LoginForm) -> Result<String, Error> {
+    assert_eq!(body.id, "TEST_ID");
+    assert_eq!(body.password, "TEST_PASSWORD");
+    Ok(foo)
+}
+
+#[tokio::test]
+async fn test_body_after_path_param() {
+    let value = warp::test::request()
+        .path("/param/foo")
+        .body(
+            serde_json::to_vec(&LoginForm {
+                id: "TEST_ID".into(),
+                password: "TEST_PASSWORD".into(),
+            })
+            .unwrap(),
+        )
+        .reply(&body_after_path_param())
+        .await
+        .into_body();
+
+    assert_eq!(value, b"foo"[..]);
+}
