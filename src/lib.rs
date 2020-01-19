2acbd60c4a3dcd08fbe7296a486d7ff01c068364
강동윤 <kdy1997.dev@gmail.com>
2020-01-19 22:10:31 +0900

    Add comments

--- src/lib.rs
+++ src/lib.rs
@@ -4,42 +4,92 @@
 //!
 //! ## `#[body]`
 //! Parses request body
-//
+//! ```rust
+//! use rweb::*;
+//! use bytes::Bytes;
+//!
+//! #[post("/body")]
+//! fn body(#[body] body: Bytes) -> Result<String, Error> {
+//!    let _ = body;
+//!    Ok(String::new())
+//! }
+//! ```
+//!
 //! ## `#[form]`
 //! Parses request body
-//
+//!
+//! ```rust
+//! use rweb::*;
+//! use serde::Deserialize;
+//!
+//! #[derive(Deserialize)]
+//! struct LoginForm {
+//!     id: String,
+//!     password: String,
+//! }
+//!
+//! #[post("/form")]
+//! fn form(#[form] body: LoginForm) -> Result<String, Error> {
+//!    Ok(serde_json::to_string(&body).unwrap())
+//! }
+//! ```
+//!
 //! ## `#[json]`
 //! Parses request body.
+//! ```rust
+//! use rweb::*;
+//! use serde::Deserialize;
+//!
+//! #[derive(Deserialize)]
+//! struct LoginForm {
+//!     id: String,
+//!     password: String,
+//! }
+//!
+//! #[post("/json")]
+//! fn json(#[json] body: LoginForm) -> String {
+//!     String::from("Ok")
+//! }
+//! ```
+//!
+//! Note that you can mix the order of parameters.
+//! ```rust
+//! #[get("/param/{a}/{b}")]
+//! fn body_between_path_params(a: u32, #[json] body: LoginForm, b: u32) ->
+//! String {     assert_eq!(body.id, "TEST_ID");
+//!     assert_eq!(body.password, "TEST_PASSWORD");
+//!     (a + b).to_string()
+//! }
+//! ```
 //!
 //! ## `#[query]`
 //! Parses query string.
-//!
 //! ```rust
 //! #[get("/")]
 //! fn use_query(#[query] qs: String) -> String {
-//!    qs
+//!     qs
 //! }
 //! ```
 //!
 //! ## `#[filter = "path_to_fn"]`
 //! Calls function.
 //!
-//! **Note**: If the callee returns `()`, you should use `()` as type of
-//! parameter.
-//!
+//! **Note**: If the callee returns `()`, you should use `()` as type. (Type
+//! alias is not allowed)
 //! ```rust
 //! use std::num::NonZeroU16;
+//! use rweb::*;
 //!
 //! #[get("/math/{num}")]
 //! fn math(num: u16, #[filter = "div_by"] denom: NonZeroU16) -> impl Reply {
-//!    rweb::reply::json(&Math {
-//!        op: format!("{} / {}", num, denom),
-//!        output: num / denom.get(),
-//!    })
+//!     rweb::reply::json(&Math {
+//!         op: format!("{} / {}", num, denom),
+//!         output: num / denom.get(),
+//!     })
 //! }
 //!
-//! fn div_by() -> impl Filter<Extract = (NonZeroU16,), Error = Rejection> +
-//! Copy {    rweb::header::<u16>("div-by").and_then(|n: u16| async move {
+//! fn div_by() -> impl Filter<Extract = (NonZeroU16,), Error = Rejection> +Copy
+//! {    rweb::header::<u16>("div-by").and_then(|n: u16| async move {
 //!        if let Some(denom) = NonZeroU16::new(n) {
 //!            Ok(denom)
 //!        } else {
