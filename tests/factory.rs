dfc111b51f5d3a91ab49c5e0b5bd6967f51b93f4
강동윤 <kdy1997.dev@gmail.com>
2020-01-21 00:05:18 +0900

    Add tests to verify that FromRequest is properly implemented

--- tests/factory.rs
+++ tests/factory.rs
@@ -1,5 +1,6 @@
 use http::StatusCode;
 use rweb::{filters::BoxedFilter, *};
+use serde::Deserialize;
 
 impl FromRequest for User {
     type Filter = BoxedFilter<(User,)>;
@@ -36,4 +37,30 @@ async fn index_test_fail() {
     let value = warp::test::request().path("/").reply(&index()).await;
 
     assert_eq!(value.status(), StatusCode::BAD_REQUEST);
+}
+
+#[derive(Deserialize)]
+struct LoginForm {
+    id: String,
+    password: String,
+}
+
+#[get("/")]
+fn json(body: Json<LoginForm>) -> String {
+    body.into_inner().id
+}
+
+#[get("/")]
+fn form(body: Form<LoginForm>) -> String {
+    body.into_inner().id
+}
+
+#[derive(Deserialize)]
+struct Pagination {
+    token: String,
+}
+
+#[get("/")]
+fn query(body: Query<Pagination>) -> String {
+    body.into_inner().token
 }
