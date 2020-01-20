38b459e808016cb266a7235d803747b170dbb319
강동윤 <kdy1997.dev@gmail.com>
2020-01-20 09:09:07 +0900

    Implemente #[data] (#3)
    This allows passing variables to actual handlers

--- src/rt.rs
+++ src/rt.rs
@@ -1,3 +1,11 @@
 pub use http::StatusCode;
 pub use std::clone::Clone;
+use std::convert::Infallible;
 pub use tokio;
+use warp::{any, Filter};
+
+pub fn provider<T: Clone + Send + Sync>(
+    data: T,
+) -> impl Filter<Extract = (T,), Error = Infallible> + Clone {
+    any().map(move || data.clone())
+}
