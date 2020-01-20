b3401c63279eac5361bb838c8c1659d299775f33
강동윤 <kdy1997.dev@gmail.com>
2020-01-20 09:10:14 +0900

    Add a test for #[data]

---
+++ tests/state.rs
@@ -0,0 +1,15 @@
+use futures::lock::Mutex;
+use rweb::*;
+use std::sync::Arc;
+
+#[derive(Clone)]
+struct Db {
+    items: Arc<Mutex<Vec<String>>>,
+}
+
+#[get("/")]
+async fn index(#[data] db: Db) -> Result<String, Rejection> {
+    let items = db.items.lock().await;
+
+    Ok(items.len().to_string())
+}
