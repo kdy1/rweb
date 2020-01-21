f26b572a402ff81a533f390caf30bdae0fcffcec
강동윤 <kdy1997.dev@gmail.com>
2020-01-21 16:04:07 +0900

    Document #[cookie]

--- src/lib.rs
+++ src/lib.rs
@@ -182,7 +182,7 @@
 //! }
 //! ```
 //!
-//! ## `#[header]`
+//! ## `#[header = "header-name"]`
 //! Value of the header.
 //! ```rust
 //! use rweb::*;
@@ -193,6 +193,20 @@
 //! }
 //! fn main() {
 //!     serve(ret_accept());
+//! }
+//! ```
+//!
+//! ## `#[cookie = "cookie-name"]`
+//! Value of the header.
+//! ```rust
+//! use rweb::*;
+//!
+//! #[get("/")]
+//! fn cookie(#[header = "sess"] sess_id: String) -> String {
+//!     sess_id
+//! }
+//! fn main() {
+//!     serve(cookie());
 //! }
 //! ```
 //!
