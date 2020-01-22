a63f3dd8e7b6bbe72096f6378804e3551662c7aa
강동윤 <kdy1997.dev@gmail.com>
2020-01-22 20:09:31 +0900

    use Arc<Mutex<Spec>>

--- src/openapi.rs
+++ src/openapi.rs
@@ -1,9 +1,10 @@
 use rweb_openapi::v3_0::Spec;
+use std::sync::{Arc, Mutex};
 use warp::{Filter, Reply};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Collector {
-    spec: Spec,
+    spec: Arc<Mutex<Spec>>,
 }
 
 pub async fn spec<F>(f: &F) -> Spec
@@ -12,17 +13,14 @@ where
     F::Extract: Reply,
 {
     let collector = Collector {
-        spec: Spec::default(),
+        spec: Default::default(),
     };
 
-    let req = warp::test::request().extension::<Collector>(collector);
-    let res = req.reply(f).await;
+    let req = warp::test::request().extension::<Collector>(collector.clone());
+    let _ = req.reply(f).await;
 
-    let (mut head, _) = res.into_parts();
-    let collector = head
-        .extensions
-        .remove::<Collector>()
-        .expect("Is collector removed?");
+    let spec = Arc::try_unwrap(collector.spec).expect("failed to unwrap Arc in Arc<Mutex<Spec>>");
+    let spec = Mutex::into_inner(spec).expect("failed to unwrap Mutex in Mutex<Spec>");
 
-    collector.spec
+    spec
 }
