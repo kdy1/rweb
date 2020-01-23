9e0d48f2fb0d5cd0b839b63908ad1f5c2cdaf9b6
강동윤 <kdy1997.dev@gmail.com>
2020-01-23 13:53:48 +0900

    WIP: quoter for Operation

--- macros/src/openapi.rs
+++ macros/src/openapi.rs
@@ -4,21 +4,27 @@ use rweb_openapi::v3_0::Operation;
 use syn::{parse2, Attribute, Expr, Lit, Meta};
 
 pub fn quote_op(op: Operation) -> Expr {
-    q!(Vars {}, {
-        rweb::openapi::Operation {
-            tags: None,
-            summary: None,
-            description: None,
-            external_docs: None,
-            operation_id: None,
-            parameters: None,
-            request_body: None,
-            responses: Default::default(),
-            callbacks: None,
-            deprecated: None,
-            servers: None,
+    q!(
+        Vars {
+            id_v: op.operation_id,
+            summary_v: op.summary,
+        },
+        {
+            rweb::openapi::Operation {
+                tags: Default::default(),
+                summary: summary_v.to_string(),
+                description: Default::default(),
+                external_docs: Default::default(),
+                operation_id: id_v.to_string(),
+                parameters: Default::default(),
+                request_body: Default::default(),
+                responses: Default::default(),
+                callbacks: Default::default(),
+                deprecated: Default::default(),
+                servers: Default::default(),
+            }
         }
-    })
+    )
     .parse()
 }
 
