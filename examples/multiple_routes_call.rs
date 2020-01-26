1a523d205002f2d2261256694c25abf6c8d82a85
Christoffer Lantz <christoffer.lantz@androidbutiken.net>
2020-01-26 08:32:56 +0100

    Data can now be injected to routes via route!-macro

--- examples/multiple_routes_call.rs
+++ examples/multiple_routes_call.rs
@@ -1,4 +1,10 @@
 use rweb::*;
+use serde::Serialize;
+#[derive(Clone, Serialize)]
+struct User {
+    id: u32,
+    name: String,
+}
 #[get("/hi")]
 fn hi() -> &'static str {
     "Hello, World!"
@@ -9,8 +15,19 @@ async fn ping(word: String) -> Result<Box<dyn warp::Reply>, warp::Rejection> {
     // a cast is needed for now, see https://github.com/rust-lang/rust/issues/60424
     Ok(Box::new(format!("pong {}", word)) as Box<dyn warp::Reply>)
 }
+#[get("/user")]
+fn display_user(#[data] user: User) -> impl Reply {
+    rweb::reply::json(&user)
+}
+
 #[tokio::main]
 async fn main() {
-    let routes = routes![hi, ping];
+    // Keep track of all connected users, key is usize, value
+    // is an event stream sender.
+    let user = User {
+        id: 1,
+        name: "Christoffer".to_string(),
+    };
+    let routes = routes![hi, ping].or(routes![user; display_user]);
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
--- src/routes.rs
+++ src/routes.rs
@@ -3,10 +3,19 @@ macro_rules! routes {
     ( $s:expr ) => {
         $s()
     };
+    ( $inject:expr; $s:expr ) => {
+        $s($inject)
+    };
     ( $s:expr, $( $x:expr ),* ) => {
             $s()
             $(
                 .or($x())
+            )*
+    };
+    ( $inject:expr; $s:expr, $( $x:expr ),* ) => {
+            $s(inject)
+            $(
+                .or($x($inject))
             )*
     };
 }
