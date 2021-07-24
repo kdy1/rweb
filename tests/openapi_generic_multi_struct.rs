c295a56f1877dc237ac981de0fae7d3c9ead371d
E-gy <balakhelisey@gmail.com>
2021-07-24 18:34:05 +0200

    (partially) converted generic multistruct test

--- tests/openapi_generic_multi_struct.rs
+++ tests/openapi_generic_multi_struct.rs
@@ -37,14 +37,12 @@ fn test_multi_generics_compile() {
             .chars()
             .all(|c| c.is_alphanumeric() || c == '.' || c == '_' || c == '-'))
     }
-    /*assert!(schemas.contains_key("One"));
+    assert!(schemas.contains_key("One"));
     assert!(schemas.contains_key("Two"));
-    assert!(schemas.contains_key("GenericStruct-_string_Opt_integer_-"));
+    assert!(schemas.contains_key("GenericStruct-string_Opt_integer-"));
     assert!(schemas.contains_key("One_Opt"));
-    assert!(schemas.contains_key("One_Map"));
-    assert!(schemas.contains_key("Two_List"));
-    assert!(schemas.contains_key("GenericStruct-_One_Opt_One_-_Opt"));
-    macro_rules! component {
+    assert!(schemas.contains_key("GenericStruct-One_Opt_One-_Opt"));
+    /*macro_rules! component {
         ($cn:expr) => {
             match schemas.get($cn) {
                 Some(ObjectOrReference::Object(s)) => s,
